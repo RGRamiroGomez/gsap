c4425c5325415c082938a9d1c01f2c1222874acc
Ramiro Gomez <ramiro.gj.97@gmail.com>
2024-06-13 16:08:51 -0300

    zarpado culeaaa

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
+gsap.registerPlugin(ScrollTrigger)
 const tl=gsap.timeline()
 const cuadro1=document.getElementsByClassName("cuadro1")
 const cuadro2=document.getElementsByClassName("cuadro2")
-tl.to(cuadro1,{duration: 1,rotate:360, x:300})
-tl.from(cuadro2,{duration: 1,rotate:-360, x:-300})
+tl.to(cuadro1,{
+    duration: 1,
+    rotate:360,
+    x:300,
+    repeat:-1,
+    yoyo:true
+})
+tl.from(cuadro2,{duration: 1,rotate:-360, x:-300,repeat:-1,
+    yoyo:true})
+gsap.to(".cuadrop2",{
+    scrollTrigger:{
+        trigger:".panel2",
+        start:"top top",
+        scrub:true,
+        pin: ".panel2",
+        toggleAction:"restart pasue resume restart"
+    },
+    y:"random(-200,200)",
+    rotation: 360,
+    duration: 3
+})
